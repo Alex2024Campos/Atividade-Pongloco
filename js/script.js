7f64f722d81ea719de444007c4eaecebd47908b5
Alex2024Campos <alex.oliveira221@etec.sp.gov.br>
2024-04-18 12:10:08 -0300

    Primeiras linhas de código

--- js/script.js
+++ js/script.js
@@ -4,4 +4,4 @@
 
 let canvasPong = document.getElementById('canvas')
 let quadro = canvasPong.getContext('2d') //Preparando o canvas para receber desenhos 2D
-let player1 =  quadro.fillRect //Vai fazer um desenho, O QUE É FILLRECT????? Fill= Preenchimento, Rect= Retangular. Vai preencher retangulamente.
+let player1 =  quadro.fillRect(100,100,50,50) //Vai fazer um desenho, O QUE É FILLRECT????? Fill= Preenchimento, Rect= Retangular. Vai preencher retangulamente. Os valores decidem a posição do elemento desenhado.
