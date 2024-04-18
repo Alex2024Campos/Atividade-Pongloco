abb5c90b4e8799c821340252b1dd59b13552e437
Alex2024Campos <alex.oliveira221@etec.sp.gov.br>
2024-04-18 12:01:31 -0300

    Primeiras linhas de código

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//socorro
+//querySelector("./#") (consulte um seletor) and getElementById[or class]('')
+//variavel para manipular o elemento html canvas
+
+let canvasPong = document.getElementById('canvas')
+
+let quadro = 
