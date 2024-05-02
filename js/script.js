11a7118339a7055963e1421ef7001338b70c2097
Alex2024Campos <alex.oliveira221@etec.sp.gov.br>
2024-05-02 12:02:33 -0300

    Mais umas edições

--- js/script.js
+++ js/script.js
@@ -7,15 +7,28 @@ let quadro = canvasPong.getContext('2d') //Preparando o canvas para receber dese
 
 
  quadro.fillStyle = '#8b8b8b' //Muda acor de todos os elementos que tiverem quadro. Adiciona cor ao fillrect, mudando assim, qual cor será preenchida pelos comandos feitos pelo programador
-let player1 =  { //Vai fazer um desenho, O QUE É FILLRECT????? Fill= Preenchimento, Rect= Retangular. Vai preencher retangulamente. Os valores decidem a posição do elemento desenhado.
+let player1 =  { //Vai fazer um desenho, O QUE É FILLRECT????? Fill= Preenchimento, Rect= Retangular. Vai preencher retangulamente. Os valores decidem a posição do elemento desenhado.12
     px:80,
     py:260,
     tx:30,
     ty:200,
 }
 
-let player2 =  quadro.fillRect(1180,260,30,200) 
-let bolinha = quadro.fillRect(1280/2-15,720/2,30,30) //Esses fill rects servem para fazer o seguinte: preencher uma parte específica da tela com o bloco ai, puxando a cor do quadro e preenchendo o espaço delimitado por isso)
+let player2 =  {
+    px:1160,
+    py:260,
+    tx:30,
+    ty:200,
+}
+
+let bolinha = {
+    px:620,
+    py:345,
+    tx:30,
+    ty:30,
+}
+
+quadro.fillRect(1280/2-20,720/2-15,30,30) //Esses fill rects servem para fazer o seguinte: preencher uma parte específica da tela com o bloco ai, puxando a cor do quadro e preenchendo o espaço delimitado por isso)
 //vale ressaltar também que os 2 primeiros valores determinam a posição do quadro e os 2 próximos valores determinam o tamanho do preenchimento.
 
 quadro.font = '30px arial'
